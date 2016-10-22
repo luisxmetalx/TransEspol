effcf6780c61d6847e0920419fe34d17bc87476d
John Cedeño <jjjohncedeno@gmail.com>
2016-10-22 13:26:29 -0500

    Creación de Autenticación Manejo de login y logout en el backend. Para esto utilice la libreria passport. Ubique nuevas cosas en el package.jason. Antes de utilizar no olvidar realizar el npm install.

--- config/policies.js
+++ config/policies.js
@@ -28,6 +28,13 @@ module.exports.policies = {
 
   // '*': true,
 
+  '*': true,
+
+  'PostController': {
+    '*': 'isAuthenticated'
+  },
+
+
   /***************************************************************************
   *                                                                          *
   * Here's an example of mapping some policies to run before a controller    *
